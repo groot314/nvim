ced694320ad59b1143d491dbb3830d732ffb3bcf
Geoffrey <groot314@gmail.com>
2025-07-31 21:18:24 -0400

    updates

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -212,6 +212,9 @@ return {
       local servers = {
         -- clangd = {},
         gopls = {},
+        ts_ls = {
+          filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
