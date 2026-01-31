c2dd54772cd2d60bd0086eea18f7032bd9e6880a
Geoffrey <groot314@gmail.com>
2026-01-30 20:39:26 -0700

    remove sqlls lint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -233,30 +233,7 @@ return {
         --
         pyright = {},
         rust_analyzer = {},
-        sqlls = {
-          settings = {
-            sqlLanguageServer = {
-              -- Lint rules (sqlint)
-              lint = {
-                rules = {
-                  ['reserved-word-case'] = { 'error', 'upper' },
-                  ['space-surrounding-operators'] = 'error',
-                },
-              },
-              -- Database connection example (uncomment and configure as needed):
-              -- connections = {
-              --   {
-              --     name = 'dev',
-              --     adapter = 'postgres',
-              --     host = 'localhost',
-              --     port = 5432,
-              --     user = 'postgres',
-              --     database = 'mydb',
-              --   },
-              -- },
-            },
-          },
-        },
+        sqlls = {},
 
         lua_ls = {
           -- cmd = { ... },
